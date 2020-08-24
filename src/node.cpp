0790df2921c8366a15c07d3600e18e42ae85668a
jkimblad <jacobki@kth.se>
2020-08-24 23:18:32 +0200

    Skeleton for node

--- src/node.cpp
+++ src/node.cpp
@@ -1,15 +1,30 @@
-#include <stdio.h>
-
 #include <vector>
 
 namespace speeddemon {
 namespace dag {
 
 class Node {
+	const unsigned int id;
+
        public:
 	std::vector<Node> children;
 
+	// Constructior
+	Node(unsigned int id) : id(id) {}
+
 	void add_child() {}
+
+	void update() {}
+
+	// Override == operator
+	bool operator==(const Node& other) {
+		// Compare id's to determine equalness
+		if (id == other.id) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 };
 }  // namespace dag
 }  // namespace speeddemon
